6e892b808b77440ff461c4c42ed72dee5763ba60
Shubham Raut <shubhamraut@KNRX-IN-C-ENG-SHUBHAMRAUT.local>
2026-09-21 11:14:29 +0530

    Add some changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     host: true, // or use '0.0.0.0'
